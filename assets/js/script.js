2936f6971d02e50ec6f8ee3508fe8d83c216e0db
Karen Pion <karenlpion@gmail.com>
2021-06-27 21:18:39 -0400

    basic styling and addint start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 console.log("java is linked")
+
+// Pseudo code
+
+// Create start button
+
+// Create a timer that counts down from 60 seconds
+
+// Create objects containing questions and answers
+
+// 
