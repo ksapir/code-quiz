024ea8b0dff04791d6f87fade72b4e3c295b5bcc
Karen Pion <karenlpion@gmail.com>
2021-06-28 02:08:29 -0400

    starting timer for quiz when start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
 console.log("java is linked")
+var timeEl = document.querySelector("#time");
+
+var startBtn = document.querySelector("#start-button");
+
+var countDown = 60;
 
 // Pseudo code
 
 // Create start button
+var startButton = document.querySelector("#start-button");
 
+startBtn.addEventListener("click", setTime);
 // Create a timer that counts down from 60 seconds
+function setTime(){
+    var timer = setInterval(function () {
+        countDown--;
+        timeEl.textContent = "Time remaing: " + countDown;
+
+        if (countDown ===0){
+            clearInterval(timer);
+        }
+}, 1000);
+}
+
+
 
 // Create objects containing questions and answers
 
