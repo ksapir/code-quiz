594da90f28bdc83d6e35ada837ce95fd6f015593
Karen Pion <karenlpion@gmail.com>
2021-06-28 02:55:49 -0400

    adding questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,43 @@ var startBtn = document.querySelector("#start-button");
 
 var countDown = 60;
 
-// Pseudo code
+var questions= {
+    q1 = {
+        question: "How many primary data types are there?";
+        answer1: "5",
+        answer2: "8",
+        answer3: "7",
+        answer4: "13"
+    },
+    q2 = {
+        question: "In Javascript, how do you print to the console?",
+        answer1: "log()",
+        answer2: "console.log()",
+        answer3: "console.print()",
+        answer4: "print()"
+    },
+    q3 ={
+        question: "What are the 2 types of functions?",
+        answer1: "declaration and expression",
+        answer2: "yelling and shouting",
+        answer3: "announcements and statements",
+        answer4: "proclamation and articulation"
+    },
+    q4= {
+        question: "What does DOM stand for?",
+        answer1: "Disorganized Ordered Methods",
+        answer2: "Digital Objectionable Moves",
+        answer3: "Databased Original Makeup",
+        answer4: "Document Object Model",
+    }
+}
 
 // Create start button
 var startButton = document.querySelector("#start-button");
 
-startBtn.addEventListener("click", setTime);
+
 // Create a timer that counts down from 60 seconds
+startBtn.addEventListener("click", setTime);
 function setTime(){
     var timer = setInterval(function () {
         countDown--;
@@ -23,8 +53,9 @@ function setTime(){
 }, 1000);
 }
 
-
-
+function quizStart () {
+    var questionStart = 
+}
 // Create objects containing questions and answers
 
 // 
