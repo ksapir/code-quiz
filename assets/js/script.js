af998b2a3df7d39b37ccf8afe68a165d7539c291
Karen Pion <karenlpion@gmail.com>
2021-06-30 01:36:45 -0400

    adding comments on javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
+// Variable for body
 var bodyEl = document.body;
 
+
+// Variables to locate id's and class on HTML
 var startPage = document.querySelector(".instruction-page");
 
 var timeEl = document.querySelector("#time");
 
 var startBtn = document.querySelector("#start-button");
 
+// Variable for score
 var counter = 0;
 
+// Variable for start of count down
 var countDown = 50;
 
 var userScore = ""
 
+// Questions and answers
 var questions= [
     {
         question: "How many primary data types are there?",
@@ -55,6 +61,7 @@ var questions= [
     },
 ];
 
+// Locating, creating, setting attributes and appending questions and answers
 var question1 = questions[0].question
 var question2 = questions[1].question
 var question3 = questions[2].question
@@ -171,41 +178,49 @@ answerBtns4bEl.style.display = "none";
 answerBtns4cEl.style.display = "none";
 answerBtns4dEl.style.display = "none";
 
+
+// <h2> that says out of time
 var theEndEl = document.createElement("h2")
 theEndEl.textContent = `Out of time! You scored: ${userScore}`
 theEndEl.setAttribute("style", "text-align: center; margin: 50px;")
 bodyEl.appendChild(theEndEl)
 theEndEl.style.display = "none";
 
+// <h2> that says what you scored
 var youScoredPage = document.createElement("h2")
 youScoredPage.textContent = `Congrats! You scored: ${userScore}`
 youScoredPage.setAttribute("style", "text-align: center; margin: 50px;")
 bodyEl.appendChild(youScoredPage)
 youScoredPage.style.display = "none"
 
+// <p> stating whether the answer was correct
 var correct = document.createElement("p")
 correct.textContent = "Correct!"
 correct.setAttribute("style", "font-size: 16px; text-align: center;")
 correct.style.display = "none"
 
-
+// <p> stating whether the answer was incorrect
 var incorrect = document.createElement("p")
 incorrect.textContent = "Incorrect"
 incorrect.setAttribute("style", "font-size: 16px; text-align: center;")
 incorrect.style.display = "none"
 
+
+// locating start button on HTML
 var startBtn = document.querySelector("#start-button");
 
 
-// Create a timer that counts down from 60 seconds
+// Creating a timer that counts down from 50 seconds
 startBtn.addEventListener("click", setTime);
+
+// Starts timer, hides start page and shows next page
 function setTime(){
     quizStart()
     startPage.style.display ="none"
     var timer = setInterval(function () {
         countDown--;
         timeEl.textContent = "Time remaing: " + countDown;
-
+// Hiding all elements on page
         if (countDown ===0){
             clearInterval(timer);
             question1El.style.display = "none";
@@ -234,13 +249,12 @@ function setTime(){
 
         theEndEl.style.display = "block"
 
-
-        }
-
+        return;
+    }
 }, 1000);
 };
 
-
+// Showing first set of question and answers
 function quizStart(){
     if (startBtn){
             question1El.style.display = "block";
@@ -264,6 +278,7 @@ function quizStart(){
     }
 }
 
+// Showing second set of qustion and answers, hides previous
 function page2(){
     if (answerBtns1aEl || answerBtns1bEl || answerBtns1cEl || answerBtns1dEl){
         question1El.style.display = "none";
@@ -292,6 +307,7 @@ function page2(){
 }
 }
 
+// Showing third set of question and answers, hides previous
 function page3(){
     if (answerBtns2aEl || answerBtns2bEl || answerBtns2cEl || answerBtns2dEl){
         question2El.style.display = "none"
@@ -320,6 +336,7 @@ function page3(){
 }
 }
 
+// Showing fourth set of question and answers, hides previous
 function page4(){
     if (answerBtns3aEl || answerBtns3bEl || answerBtns3cEl || answerBtns3dEl){
         question3El.style.display = "none"
@@ -339,6 +356,7 @@ function page4(){
         answerBtns4cEl.addEventListener("click", page5)
         answerBtns4dEl.style.display = "block"
         answerBtns4dEl.addEventListener("click", page5)
+
     if (answerBtns4dEl){
         correct.style.display = "block"
     } else {
@@ -346,6 +364,8 @@ function page4(){
     }   
 }
 }
+
+// Hides previous
 function page5(){
     if (answerBtns4aEl || answerBtns4bEl || answerBtns4cEl || answerBtns4dEl){
         question4El.style.display = "none"
@@ -357,45 +377,7 @@ function page5(){
         incorrect.style.display = "none"
 
         theEndEl.style.display = "none"
-        youScoredPage.styleisplay = "block"
-
-    if (answerBtns4El){
-         correct.style.display = "block"
-         counter += 10
-    } else {
-        incorrect.style.display = "block"
-    }   
-}
-}
-
-function page6(){
+        youScoredPage.style.display = "block"
 
 }
-
-// TODO: Create objects containing questions and answers
-
-// TODO: Create a basic html page which has a start page that includes a button to start our quiz, a view highscores button, and a timer.
-
-//TODO: In our JS - create questions and answers that we can iterate through (object) to display the user array.
-
-//TODO:Set up some global variables, counter, timer, score, highscores
-
-//Counter will dictate where we are in our questions array - counter ++
-// Timer will use setInterval so that we hae a functioning timer that counts down that we can decrement if ther user gets an incorrect answer
-//Score save this because the user and their score should be saved in local storage
-
-// When the start button is clicked, the start screen hides and the timer starts then the questions show up.
-
-//Use javascript to change our CSS to tell the page based on whether we have started what shows on the screen
-//set up the timer so that it runs on the start of the game
-// click handler on the start button
-// start the timer and make sure that the quiz ends at 0 seconds
-// Change the CSS on all of the required HTML elements, so that we get what we want on the page
-// display the first object in the array on the page
-// set up click handlers on the answer buttons so that when a user selects an answer we chan check a few things
-// check if the answer is right or wrong, increment to the next question on the page if we are at the end of the our array we want to end the quiz, incorrect answer decrement time, increment counter
-// Show the user if they got the question correct or incorrect underneath the answers
-    // use set timeout to change classes to show the user right or wrong
-//At the end of the quiz, hide the questions and show the user score (created this var earlier), use the score and the user initials to save that info in local storage.
-//create an array of objects [user: name, score:] each time we pull the info from local storage, we set our highscores to either info there || []
-// Hide the user input screen and show the highscores to the user and ask them if they want to take the quiz again.
+}
